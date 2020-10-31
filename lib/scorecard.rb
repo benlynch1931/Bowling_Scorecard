b432c2ec815cd55074cea4f0c3cbe3a7d7a99689
Ben <ben.lynch1931@gmail.com>
2020-10-31 14:37:25 +0000

    UNIT - ScoreCard: Red/Green 'scorecard displays complex game with strike in frame10'

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,18 +1,18 @@
-class ScoreCard
+# frozen_string_literal: true
 
+class ScoreCard
   def initialize
     @score = 0
     @frames = []
-    10.times { |each|
+    10.times do |each|
       @frames.push(
         {
           frame: each + 1,
-          roll_one: {pins: '  ', score: 0, bonus: '', bonus_score: 0, notes: ''},
-          roll_two: {pins: '  ', score: 0, bonus: '', bonus_score: 0, notes: ''}
-          }
-        )
-    }
-
+          roll_one: { pins: '  ', score: 0, bonus: ' nil  ', bonus_score: 0, notes: '' },
+          roll_two: { pins: '  ', score: 0, bonus: ' nil  ', bonus_score: 0, notes: '' }
+        }
+      )
+    end
   end
 
   def controller
@@ -20,52 +20,77 @@ class ScoreCard
       user_roll_one = gets.chomp
       @frames[round][:roll_one][:pins] = user_roll_one.to_i
       @frames[round][:roll_one][:score] = user_roll_one.to_i
+      # puts @frames[round][:roll_one][:score] if round == 9
       if user_roll_one == '10'
         @frames[round][:roll_one][:bonus] = 'strike'
-        next
+        # puts "#{round + 1}: STRIKE"
+        next unless round == 9
       else
         @frames[round][:roll_one][:pins] = user_roll_one.to_i
         @frames[round][:roll_one][:score] = user_roll_one.to_i
       end
 
       user_roll_two = gets.chomp
-      if user_roll_two.to_i + user_roll_one.to_i == 10
-        @frames[round][:roll_two][:bonus] = 'spare'
+      # puts "#{round + 1} : #{user_roll_one.to_i + user_roll_two.to_i}"
+      @frames[round][:roll_two][:bonus] = 'spare ' if user_roll_one.to_i + user_roll_two.to_i == 10
+      @frames[round][:roll_two][:pins] = user_roll_two.to_i
+      @frames[round][:roll_two][:score] = user_roll_two.to_i
+      # puts "#{@frames[round][:roll_one]}" unless round == 9
+      # puts "#{@frames[round][:roll_two]}" unless round == 9
+      next unless round == 9
+      if @frames[round][:roll_one][:bonus] == 'strike' || @frames[round][:roll_two][:bonus] == 'spare '
+        user_roll_three = gets.chomp
+        # puts "#{@frames[round][:roll_one]}"
+        # puts "#{@frames[round][:roll_two]}"
+        @frames[round][:roll_three] = { pins: user_roll_three.to_i, score: user_roll_three.to_i, bonus: ' nil  ', bonus_score: 0, notes: '' } unless user_roll_three == ''
       end
-        @frames[round][:roll_two][:pins] = user_roll_two.to_i
-        @frames[round][:roll_two][:score] = user_roll_two.to_i
     end
   end
 
   def scorecard
-    print %Q(
-  | frame | roll | knocked pins | total score | bonus |
-  |-------|------|--------------|-------------|-------|
+    # 9.times { |ea| puts "#{ea + 1}: #{@frames[ea][:roll_one]}\n#{@frames[ea][:roll_two]}"}
+    # puts "#{10}: #{@frames[9][:roll_one]}\n#{@frames[9][:roll_two]}\n#{@frames[9][:roll_three]}"
+    print %(
+  | frame | roll | knocked pins | total score |  bonus  |
+  |-------|------|--------------|-------------|---------|
 )
     @frames.each_with_index do |frame, idx|
-      bonus(idx)
-      @score += (frame[:roll_one][:score] + frame[:roll_two][:score] + frame[:roll_one][:bonus_score] + frame[:roll_two][:bonus_score])
+      bonus(idx) if idx < 9
+      extra = 0
+      extra = frame[:roll_three][:score] if frame[:roll_three]
+      @score += (frame[:roll_one][:score] + frame[:roll_two][:score] + frame[:roll_one][:bonus_score] + frame[:roll_two][:bonus_score]) + extra
       space = ' ' if frame[:frame] < 10
       score_space = ' ' if @score < 100
       score_space = '  ' if @score < 10
-      pins_space = ' ' if frame[:roll_one][:pins].kind_of?(Integer) && frame[:roll_one][:pins] < 10
-      puts "  |  #{space}#{frame[:frame]}   |   1  |     #{pins_space}#{frame[:roll_one][:pins]}       |             |  nil  |"
-      puts "  |  #{space}#{frame[:frame]}   |   2  |     #{pins_space}#{frame[:roll_two][:pins]}       |    #{score_space}#{score}      |  nil  |"
+      pins_space = ' ' if frame[:roll_one][:pins].is_a?(Integer) && frame[:roll_one][:pins] < 10
+      bonus_str = frame[:roll_one][:bonus]
+      puts "  |  #{space}#{frame[:frame]}   |   1  |     #{pins_space}#{frame[:roll_one][:pins]}       |             |  #{bonus_str} |"
+      bonus_str = frame[:roll_two][:bonus]
+      if frame[:roll_three]
+        pins_space = ' ' if frame[:roll_two][:pins].is_a?(Integer) && frame[:roll_two][:pins] < 10
+        puts "  |  #{space}#{frame[:frame]}   |   2  |     #{pins_space}#{frame[:roll_two][:pins]}       |             |  #{bonus_str} |"
+        bonus_str = frame[:roll_three][:bonus]
+        pins_space = ' ' if frame[:roll_three][:pins].is_a?(Integer) && frame[:roll_three][:pins] < 10
+        puts "  |  #{space}#{frame[:frame]}   |   3  |     #{pins_space}#{frame[:roll_three][:pins]}       |    #{score_space}#{score}      |  #{bonus_str} |"
+      else
+        puts "  |  #{space}#{frame[:frame]}   |   2  |     #{pins_space}#{frame[:roll_two][:pins]}       |    #{score_space}#{score}      |  #{bonus_str} |"
+      end
     end
   end
 
   def bonus(idx)
     if @frames[idx][:roll_one][:bonus] == 'strike'
-      strike_bonus(idx+1)
+      strike_bonus(idx + 1)
       @frames[idx][:roll_one][:bonus_score] = (@frames[idx + 1][:roll_one][:score] + @frames[idx + 1][:roll_two][:score])
-    elsif @frames[idx][:roll_two][:bonus] == 'spare'
+    elsif @frames[idx][:roll_two][:bonus] == 'spare '
       @frames[idx][:roll_one][:bonus_score] = @frames[idx + 1][:roll_one][:score]
     end
   end
 
   def strike_bonus(idx)
     if @frames[idx][:roll_one][:bonus] == 'strike'
-      strike_bonus(idx+1)
+      return if idx == 9
+      strike_bonus(idx + 1)
       @frames[idx][:roll_one][:bonus_score] = (@frames[idx + 1][:roll_one][:score] + @frames[idx + 1][:roll_two][:score])
     end
   end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -1,58 +1,83 @@
+# frozen_string_literal: true
+
 require 'scorecard'
 
 describe ScoreCard do
-
-  simple_scorecard = %Q(
-  | frame | roll | knocked pins | total score | bonus |
-  |-------|------|--------------|-------------|-------|
-  |   1   |   1  |      4       |             |  nil  |
-  |   1   |   2  |      5       |      9      |  nil  |
-  |   2   |   1  |      3       |             |  nil  |
-  |   2   |   2  |      1       |     13      |  nil  |
-  |   3   |   1  |      4       |             |  nil  |
-  |   3   |   2  |      5       |     22      |  nil  |
-  |   4   |   1  |      3       |             |  nil  |
-  |   4   |   2  |      1       |     26      |  nil  |
-  |   5   |   1  |      4       |             |  nil  |
-  |   5   |   2  |      5       |     35      |  nil  |
-  |   6   |   1  |      3       |             |  nil  |
-  |   6   |   2  |      1       |     39      |  nil  |
-  |   7   |   1  |      4       |             |  nil  |
-  |   7   |   2  |      5       |     48      |  nil  |
-  |   8   |   1  |      3       |             |  nil  |
-  |   8   |   2  |      1       |     52      |  nil  |
-  |   9   |   1  |      4       |             |  nil  |
-  |   9   |   2  |      5       |     61      |  nil  |
-  |  10   |   1  |      3       |             |  nil  |
-  |  10   |   2  |      1       |     65      |  nil  |
+  simple_scorecard = %(
+  | frame | roll | knocked pins | total score |  bonus  |
+  |-------|------|--------------|-------------|---------|
+  |   1   |   1  |      4       |             |   nil   |
+  |   1   |   2  |      5       |      9      |   nil   |
+  |   2   |   1  |      3       |             |   nil   |
+  |   2   |   2  |      1       |     13      |   nil   |
+  |   3   |   1  |      4       |             |   nil   |
+  |   3   |   2  |      5       |     22      |   nil   |
+  |   4   |   1  |      3       |             |   nil   |
+  |   4   |   2  |      1       |     26      |   nil   |
+  |   5   |   1  |      4       |             |   nil   |
+  |   5   |   2  |      5       |     35      |   nil   |
+  |   6   |   1  |      3       |             |   nil   |
+  |   6   |   2  |      1       |     39      |   nil   |
+  |   7   |   1  |      4       |             |   nil   |
+  |   7   |   2  |      5       |     48      |   nil   |
+  |   8   |   1  |      3       |             |   nil   |
+  |   8   |   2  |      1       |     52      |   nil   |
+  |   9   |   1  |      4       |             |   nil   |
+  |   9   |   2  |      5       |     61      |   nil   |
+  |  10   |   1  |      3       |             |   nil   |
+  |  10   |   2  |      1       |     65      |   nil   |
 )
-  complex_scorecard = %Q(
-  | frame | roll | knocked pins | total score | bonus |
-  |-------|------|--------------|-------------|-------|
-  |   1   |   1  |      5       |             |  nil  |
-  |   1   |   2  |      5       |     13      |  nil  |
-  |   2   |   1  |      3       |             |  nil  |
-  |   2   |   2  |      1       |     17      |  nil  |
-  |   3   |   1  |     10       |             |  nil  |
-  |   3   |   2  |              |     37      |  nil  |
-  |   4   |   1  |      5       |             |  nil  |
-  |   4   |   2  |      5       |     50      |  nil  |
-  |   5   |   1  |      3       |             |  nil  |
-  |   5   |   2  |      1       |     54      |  nil  |
-  |   6   |   1  |     10       |             |  nil  |
-  |   6   |   2  |              |     74      |  nil  |
-  |   7   |   1  |      5       |             |  nil  |
-  |   7   |   2  |      5       |     87      |  nil  |
-  |   8   |   1  |      3       |             |  nil  |
-  |   8   |   2  |      1       |     91      |  nil  |
-  |   9   |   1  |     10       |             |  nil  |
-  |   9   |   2  |              |    102      |  nil  |
-  |  10   |   1  |      0       |             |  nil  |
-  |  10   |   2  |      1       |    103      |  nil  |
+  complex_scorecard = %(
+  | frame | roll | knocked pins | total score |  bonus  |
+  |-------|------|--------------|-------------|---------|
+  |   1   |   1  |      5       |             |   nil   |
+  |   1   |   2  |      5       |     13      |  spare  |
+  |   2   |   1  |      3       |             |   nil   |
+  |   2   |   2  |      1       |     17      |   nil   |
+  |   3   |   1  |     10       |             |  strike |
+  |   3   |   2  |              |     37      |   nil   |
+  |   4   |   1  |      5       |             |   nil   |
+  |   4   |   2  |      5       |     50      |  spare  |
+  |   5   |   1  |      3       |             |   nil   |
+  |   5   |   2  |      1       |     54      |   nil   |
+  |   6   |   1  |     10       |             |  strike |
+  |   6   |   2  |              |     74      |   nil   |
+  |   7   |   1  |      5       |             |   nil   |
+  |   7   |   2  |      5       |     87      |  spare  |
+  |   8   |   1  |      3       |             |   nil   |
+  |   8   |   2  |      1       |     91      |   nil   |
+  |   9   |   1  |     10       |             |  strike |
+  |   9   |   2  |              |    102      |   nil   |
+  |  10   |   1  |      0       |             |   nil   |
+  |  10   |   2  |      1       |    103      |   nil   |
+)
+  complex_scorecard2 = %(
+  | frame | roll | knocked pins | total score |  bonus  |
+  |-------|------|--------------|-------------|---------|
+  |   1   |   1  |      5       |             |   nil   |
+  |   1   |   2  |      5       |     13      |  spare  |
+  |   2   |   1  |      3       |             |   nil   |
+  |   2   |   2  |      1       |     17      |   nil   |
+  |   3   |   1  |     10       |             |  strike |
+  |   3   |   2  |              |     37      |   nil   |
+  |   4   |   1  |      5       |             |   nil   |
+  |   4   |   2  |      5       |     50      |  spare  |
+  |   5   |   1  |      3       |             |   nil   |
+  |   5   |   2  |      1       |     54      |   nil   |
+  |   6   |   1  |     10       |             |  strike |
+  |   6   |   2  |              |     74      |   nil   |
+  |   7   |   1  |      5       |             |   nil   |
+  |   7   |   2  |      5       |     87      |  spare  |
+  |   8   |   1  |      3       |             |   nil   |
+  |   8   |   2  |      1       |     91      |   nil   |
+  |   9   |   1  |     10       |             |  strike |
+  |   9   |   2  |              |    112      |   nil   |
+  |  10   |   1  |     10       |             |  strike |
+  |  10   |   2  |      1       |             |   nil   |
+  |  10   |   3  |      5       |    128      |   nil   |
 )
 
   context ' #initialize' do
-
     it 'instantiates with a score variable' do
       expect(subject.score).to eq 0
     end
@@ -61,20 +86,18 @@ describe ScoreCard do
       frames = subject.frames
       frames.each_with_index do |frame, idx|
         expect(frame[:frame]).to eq(idx + 1)
-        expect(frame[:roll_one]).to eq({pins: '  ', score: 0, bonus: '', bonus_score: 0, notes: ''})
-        expect(frame[:roll_one]).to eq({pins: '  ', score: 0, bonus: '', bonus_score: 0, notes: ''})
+        expect(frame[:roll_one]).to eq({ pins: '  ', score: 0, bonus: ' nil  ', bonus_score: 0, notes: '' })
+        expect(frame[:roll_one]).to eq({ pins: '  ', score: 0, bonus: ' nil  ', bonus_score: 0, notes: '' })
       end
     end
   end
 
   context ' #controller' do
-
     it 'responds when called' do
       expect(subject).to respond_to(:controller)
     end
 
     context 'when strike rolled' do
-
       it 'adds 10 points to the frames array' do
         allow(subject).to receive(:gets).and_return('10')
         subject.controller
@@ -99,7 +122,7 @@ describe ScoreCard do
 
     context 'roll_two' do
       it 'adds pins and score to frames array' do
-        allow(subject).to receive(:gets).and_return('0','5')
+        allow(subject).to receive(:gets).and_return('0', '5')
         subject.controller
         expect(subject.frames[0][:roll_two][:pins]).to eq 5
         expect(subject.frames[0][:roll_two][:score]).to eq 5
@@ -110,29 +133,31 @@ describe ScoreCard do
       it 'iterates through frames like a real game' do
         allow(subject).to receive(:gets).and_return('4', '5', '3', '1')
         subject.controller
-        expect(subject.frames[0][:roll_one]).to eq({pins: 4, score: 4, bonus: '', bonus_score: 0, notes: ''})
-        expect(subject.frames[0][:roll_two]).to eq({pins: 5, score: 5, bonus: '', bonus_score: 0, notes: ''})
-        expect(subject.frames[1][:roll_one]).to eq({pins: 3, score: 3, bonus: '', bonus_score: 0, notes: ''})
-        expect(subject.frames[1][:roll_two]).to eq({pins: 1, score: 1, bonus: '', bonus_score: 0, notes: ''})
+        expect(subject.frames[0][:roll_one]).to eq({ pins: 4, score: 4, bonus: ' nil  ', bonus_score: 0, notes: '' })
+        expect(subject.frames[0][:roll_two]).to eq({ pins: 5, score: 5, bonus: ' nil  ', bonus_score: 0, notes: '' })
+        expect(subject.frames[1][:roll_one]).to eq({ pins: 3, score: 3, bonus: ' nil  ', bonus_score: 0, notes: '' })
+        expect(subject.frames[1][:roll_two]).to eq({ pins: 1, score: 1, bonus: ' nil  ', bonus_score: 0, notes: '' })
       end
     end
   end
 
   context ' #print_scorecard' do
-    it "prints all info about frames. NO SPARE/STRIKE" do
-      allow(subject).to receive(:gets).and_return('4', '5', '3', '1', '4', '5', '3', '1', '4', '5', '3', '1', '4', '5', '3', '1', '4', '5', '3', '1')
+    it 'prints all info about frames. NO SPARE/STRIKE' do
+      allow(subject).to receive(:gets).and_return('4', '5', '3', '1', '4', '5', '3', '1', '4', '5', '3', '1', '4', '5', '3', '1', '4', '5', '3', '1', '')
       subject.controller
       expect { subject.scorecard }.to output(simple_scorecard).to_stdout
     end
 
     it 'prints all info about frames. INCL. SPARE/STRIKE' do
-      allow(subject).to receive(:gets).and_return('5', '5', '3', '1', '10', '5', '5', '3', '1', '10', '5', '5', '3', '1', '10', '0', '1')
+      allow(subject).to receive(:gets).and_return('5', '5', '3', '1', '10', '5', '5', '3', '1', '10', '5', '5', '3', '1', '10', '0', '1', '')
       subject.controller
       expect { subject.scorecard }.to output(complex_scorecard).to_stdout
     end
+
+    it 'prints all info when strike in frame10' do
+      allow(subject).to receive(:gets).and_return('5', '5', '3', '1', '10', '5', '5', '3', '1', '10', '5', '5', '3', '1', '10', '10', '1', '5')
+      subject.controller
+      expect { subject.scorecard }.to output(complex_scorecard2).to_stdout
+    end
   end
-
-
-
-
 end
